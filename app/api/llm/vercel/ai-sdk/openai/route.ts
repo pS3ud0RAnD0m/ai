f4991cc8d9d4884fe9711618df174e050fa4ce87
pS3ud0RAnD0m <maximushyde@gmail.com>
2024-10-02 21:31:41 -0400

    Reorganized pages and implemented basic use of vercel/ai sdk

---
+++ app/api/llm/vercel/ai-sdk/openai/route.ts
@@ -0,0 +1,22 @@
+import { streamText } from 'ai'
+import { openai } from '@ai-sdk/openai'
+
+export async function POST(req: Request) {
+    // Extract the `messages` from the body of the request
+    const { messages } = await req.json();
+
+    // Get a language model
+    const model = openai('gpt-4o')
+
+    // Call the language model with the prompt
+    const result = await streamText({
+        model,
+        messages,
+        maxTokens: 1024,
+        temperature: 0.7,
+        topP: 1,
+    })
+
+    // Respond with a streaming response
+    return result.toAIStreamResponse()
+}
